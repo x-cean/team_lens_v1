24c54b6cca16f9ba8f2fa1bd66d65618a18de871
xiao <194778728+x-cean@users.noreply.github.com>
2025-07-15 19:52:22 +0200

    route home v0

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
+from routes import home
+
+
+app = FastAPI()
+
+
+# Middleware (optional CORS), for frontend
+# TODO: in case there's react or other front end
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://spaeholderforfuturefrontendnotexistedyethaha.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(home.router)
