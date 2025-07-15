669d1ea6555c318362fe2ad7ee8abe0c92a2937f
xiao <194778728+x-cean@users.noreply.github.com>
2025-07-15 23:53:11 +0200

    open ai

---
+++ services/llm/open_ai.py
@@ -0,0 +1,32 @@
+import openai
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+def get_response_from_openai(user_prompt):
+
+    client = openai.OpenAI(api_key=API_KEY)
+
+    # Specify the model to use
+    model = "gpt-4o-mini"
+
+    # Generate a response using the OpenAI API
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "assistant",
+             "content": "You are a helpful work assistant, "
+                        "good at finding work-related information from lots of documents"
+                        "and give simple, clear and professional answer."},
+            {"role": "user", "content": user_prompt}
+        ],
+        temperature=2,
+        max_tokens=150
+    )
+
+    # Return the generated text
+    return response.choices[0].message.content
