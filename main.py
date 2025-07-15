233e8ac4e769e0635b507357e7db132e03c57407
xiao <194778728+x-cean@users.noreply.github.com>
2025-07-15 21:12:44 +0200

    homepage html sketched

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
+import os
 
 from routes import home
 
 
 app = FastAPI()
 
+# TODO: just question, without this line it still works, what is this for?
+# Mount static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Middleware (optional CORS), for frontend
 # TODO: in case there's react or other front end
--- routes/home.py
+++ routes/home.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import os
 
 
+templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "..", "templates"))
 router = APIRouter()
 
 
-@router.get("/")
-def home():
-    return {"message": "Welcome to TeamLens v1 API!"}
+@router.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
