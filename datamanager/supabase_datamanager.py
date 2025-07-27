29a6344f209e939f44b6f354e136354dfacbb05b
xiao <194778728+x-cean@users.noreply.github.com>
2025-07-27 19:51:01 +0200

    supabase datamanager

--- datamanager/supabase_datamanager.py
+++ datamanager/supabase_datamanager.py
@@ -18,16 +18,24 @@ def sterilize_for_json(data: dict) -> dict:
             result[key] = value
     return result
 
+
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise RuntimeError("Please set SUPABASE_URL and SUPABASE_KEY environment variables")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-def create_user(user: User) -> User:
-    user_info = sterilize_for_json(user.model_dump(mode='python'))
-    user_entry = supabase.table('user').insert(user_info).execute()
-    return user_entry
 
-user_example = User(name='user_two', email='user_two@example.com')
-create_user(user_example)
+class SupabaseDataManager(DataManagerInterface):
+    def __init__(self, client: Client):
+        if client is None:
+            raise ValueError("Supabase client cannot be None")
+        self.client = client
 
+    def create_user(self, user: User):
+        user_info = sterilize_for_json(user.model_dump(mode='python'))
+        response = self.client.table('user').insert(user_info).execute()
+        return response.data
+
+user_example = User(name='user_three', email='user_three@example.com')
+data_manager = SupabaseDataManager(supabase)
+print(data_manager.create_user(user_example))
