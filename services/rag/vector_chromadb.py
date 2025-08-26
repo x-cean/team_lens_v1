becc04496ff2aca7ffb5ca2ec9198acaf6cad983
xiao <194778728+x-cean@users.noreply.github.com>
2025-08-26 16:46:57 +0200

    working on chroma

--- services/rag/vector_chromadb.py
+++ services/rag/vector_chromadb.py
@@ -4,14 +4,21 @@ import dotenv
 from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
 from config import OPENAI_API_KEY
 
+# establish a persistent client for test purposes
 chroma_client = chromadb.PersistentClient(path="/data/database/chroma")
 
+# set up the embedding function
+embedding_function = OpenAIEmbeddingFunction(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    model_name="text-embedding-3-small"
+)
+
+# create a collection
 collection = chroma_client.create_collection(
     name="test_collection",
-    embedding_function=OpenAIEmbeddingFunction(
-        api_key=os.getenv("OPENAI_API_KEY"),
-        model_name="text-embedding-3-small"
-    )
+    embedding_function=embedding_function
 )
+
+
 
 
