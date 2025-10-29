15ab1713fcaa83247a2f0f10a785a9b11531df76
xiao <194778728+x-cean@users.noreply.github.com>
2025-10-29 17:35:56 +0100

    postgres did not work but supabse client worked

--- datamanager/sql_database_init.py
+++ datamanager/sql_database_init.py
@@ -6,29 +6,28 @@ from sqlmodel import SQLModel, create_engine, Session, select
 from fastapi import Depends
 from typing import Annotated
 
+SESSION_POOLER_SITE = f"postgresql+psycopg2://postgres.{SUPABASE_DB_STRING}:{SUPABASE_DB_PASSWORD}@aws-1-eu-north-1.pooler.supabase.com:5432/postgres"
+DIRECT_CONNECTION_SITE = f"postgresql://postgres:{SUPABASE_DB_PASSWORD}@db.{SUPABASE_URL_2}:5432/postgres"
+
 
 # sql
 def fastapi_postgresql_init():
     # create tables if not exist
-    postgres_engine = create_engine(f"postgresql://postgres:{SUPABASE_DB_PASSWORD}@db.{SUPABASE_URL_2}:5432/postgres")
+    postgres_engine = create_engine(DIRECT_CONNECTION_SITE)
     SQLModel.metadata.create_all(postgres_engine)
     with Session(postgres_engine) as postgres_session:
         yield postgres_session
 
 def postgresql_init():
-    direct_connection_site = f"postgresql://postgres:{SUPABASE_DB_PASSWORD}@db.{SUPABASE_URL_2}:5432/postgres"
-    session_pooler_site = \
-        (f"postgresql://postgres.{SUPABASE_DB_STRING}:{SUPABASE_DB_PASSWORD}"
-         f"@aws-1-eu-north-1.pooler.supabase.com:5432/postgres")
-    postgres_engine = create_engine(session_pooler_site)
+    postgres_engine = create_engine(DIRECT_CONNECTION_SITE)
     SQLModel.metadata.create_all(postgres_engine)
     postgres_session = Session(postgres_engine)
     return postgres_session
 
 # supabase
 def supabase_init():
-    # create tables if not exist
-    postgresql_init()
+    # create tables if not exist, i uncommented it cause postgresql is giving error atm
+    # postgresql_init()
     if not SUPABASE_URL or not SUPABASE_KEY:
         raise RuntimeError("Please set SUPABASE_URL and SUPABASE_KEY environment variables")
     # initiate client
