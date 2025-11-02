79bfbcacbef70124fdf7ff0f712a7bf6e26f6d51
xiao <194778728+x-cean@users.noreply.github.com>
2025-11-02 17:42:49 +0100

    function: collect info from docling docs

--- services/rag/parser_1.py
+++ services/rag/parser_1.py
@@ -22,35 +22,35 @@ def docs_to_texts(docs: list) -> list:
     return texts
 
 
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    try:
-        with open(pdf_path, "rb") as file:
-            reader = PdfReader(file)
-            page_texts = [page.extract_text() or "" for page in reader.pages]
-        text = " ".join(page_texts)
-        return text.replace("\n", "")
-    except FileNotFoundError:
-        return "File not found"
-
-
-def extract_text_from_pdf_like_object(pdf_like_object):
-    text = ""
-    try:
-        reader = PdfReader(pdf_like_object)
-        page_texts = [page.extract_text() or "" for page in reader.pages]
-        text = " ".join(page_texts)
-        return text.replace("\n", " ")
-    except FileNotFoundError:
-        return "File not found"
-
-
-def extract_text_from_txt(file_path):
-    try:
-        with open(file_path, "r", encoding="utf-8") as f:
-            return f.read()
-    except FileNotFoundError:
-        return "File not found"
+# def extract_text_from_pdf(pdf_path):
+#     text = ""
+#     try:
+#         with open(pdf_path, "rb") as file:
+#             reader = PdfReader(file)
+#             page_texts = [page.extract_text() or "" for page in reader.pages]
+#         text = " ".join(page_texts)
+#         return text.replace("\n", "")
+#     except FileNotFoundError:
+#         return "File not found"
+#
+#
+# def extract_text_from_pdf_like_object(pdf_like_object):
+#     text = ""
+#     try:
+#         reader = PdfReader(pdf_like_object)
+#         page_texts = [page.extract_text() or "" for page in reader.pages]
+#         text = " ".join(page_texts)
+#         return text.replace("\n", " ")
+#     except FileNotFoundError:
+#         return "File not found"
+#
+#
+# def extract_text_from_txt(file_path):
+#     try:
+#         with open(file_path, "r", encoding="utf-8") as f:
+#             return f.read()
+#     except FileNotFoundError:
+#         return "File not found"
 
 
 # example_pdf_path = "../../data/test_examples/Nature_moon.pdf"
--- services/rag/vectordb_chroma_persistent.py
+++ services/rag/vectordb_chroma_persistent.py
@@ -1,23 +1,41 @@
 import chromadb
 from chromadb.utils.embedding_functions import OpenAIEmbeddingFunction
+from datetime import datetime
 from team_lens_v1.config import OPENAI_API_KEY
 
 
 CHROMA_LOCAL_DATABASE_PATH = "/data/database/chroma_persistent"
 
 
+def collect_ids_and_documents_and_metadata_from_docs(docs, user_id: str | None=None):
+    ids = []
+    documents = []
+    metadata_list = []
+    for i, doc in enumerate(docs):
+        ids.append(f"{i}")
+        documents.append(doc.page_content)
+        metadata_list.append({"username": user_id, "source": doc.metadata.get("source", "unknown")})
+    return ids, documents, metadata_list
+
 def establish_chroma_persistent_client(user_id: str):
     user_chroma_client = chromadb.PersistentClient(path=f"{CHROMA_LOCAL_DATABASE_PATH}/{user_id}")
     return user_chroma_client
 
+
 def create_collection_with_openai_embedding(user_id: str, collection_name: str,
-                                            doc_ids: list[str], doc_documents: list[str]):
+                                            file_path: str,
+                                            doc_ids: list[str], doc_documents: list[str],
+                                            metadata_list: list[dict] = None):
     client = establish_chroma_persistent_client(user_id)
     collection = client.get_or_create_collection(
         name=collection_name,
         embedding_function=OpenAIEmbeddingFunction(
             model_name="text-embedding-3-small"
-        )
+        ),
+        metadata={
+            "created_by": user_id,
+            "created_at": str(datetime.now()),
+            "source": file_path}
     )
     collection.add(
         ids=doc_ids,
@@ -25,11 +43,13 @@ def create_collection_with_openai_embedding(user_id: str, collection_name: str,
     )
     return collection
 
+
 def add_documents_to_collection(collection, ids, documents):
     collection.add(
         ids=ids,
         documents=documents
     )
+    return collection
 
 
 
