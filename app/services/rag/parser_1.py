560deb0d4eab97e25e4fdb44c76c74fe4df5a3d7
xiao <194778728+x-cean@users.noreply.github.com>
2025-12-02 16:08:50 +0100

    cleaned unused functions in parser_1

--- app/services/rag/parser_1.py
+++ app/services/rag/parser_1.py
@@ -3,7 +3,6 @@ Langchain docling file loader and text extractor utilities
 """
 
 
-from pypdf import PdfReader
 from langchain_docling import DoclingLoader
 
 
@@ -25,39 +24,3 @@ def docs_to_texts(docs: list) -> list:
     """
     texts = [doc.page_content for doc in docs]
     return texts
-
-
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    try:
-        with open(pdf_path, "rb") as file:
-            reader = PdfReader(file)
-            page_texts = [page.extract_text() or "" for page in reader.pages]
-        text = " ".join(page_texts)
-        return text.replace("\n", "")
-    except FileNotFoundError:
-        return "File not found"
-
-#
-def extract_text_from_pdf_like_object(pdf_like_object):
-    text = ""
-    try:
-        reader = PdfReader(pdf_like_object)
-        page_texts = [page.extract_text() or "" for page in reader.pages]
-        text = " ".join(page_texts)
-        return text.replace("\n", " ")
-    except FileNotFoundError:
-        return "File not found"
-#
-#
-def extract_text_from_txt(file_path):
-    try:
-        with open(file_path, "r", encoding="utf-8") as f:
-            return f.read()
-    except FileNotFoundError:
-        return "File not found"
-
-
-# example_pdf_path = "../../my_data/test_examples/Nature_moon.pdf"
-# text = extract_text_from_pdf(example_pdf_path)
-# print(text)
